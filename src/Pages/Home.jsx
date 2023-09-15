76871afaa3792f56f3d564b3e552d137c4626960
Martin Luzardo <107727637+Martinluzado@users.noreply.github.com>
2023-09-14 21:29:20 -0300

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
       <nav>
          <div className='PascalSearch'>
           <input
-            autoFocus="none"
+            autoFocus='none'
             className='Imputheader'
             type='text' 
             placeholder='Buscar'
