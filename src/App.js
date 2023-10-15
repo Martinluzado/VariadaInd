2c8e7d8c0eef99fb457b11a033beedcdd5085e08
Martin Luzardo <107727637+Martinluzado@users.noreply.github.com>
2023-10-15 12:42:02 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import './App.css';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from './Pages/Home';
-import SobreNosotros from './Components/SobreNosotros';
-import MetodosDePagos from './Components/MetodosDePagos';
-import MetodosDeEnvios from './Components/MetodosDeEnvios';
-import TarjetaEntera from './Components/TarjetaEntera';
-import ListaProductos from './Data/Datos'
-
-
+// App.js
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SobreNosotros from "./Components/SobreNosotros";
+import MetodosDePagos from "./Components/MetodosDePagos";
+import MetodosDeEnvios from "./Components/MetodosDeEnvios";
+import TarjetaEntera from "./Components/TarjetaEntera";
+import ListaProductos from "./Data/Datos";
+import "./App.css";
 
 function App() {
-
-
   return (
     <div className="App">
       <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route
-              path='/:ProductosNombre'
-              element={<TarjetaEntera infoRopa={ListaProductos}/>}>
-              {" "}
-            </Route>
-            <Route path='/SobreNosotros' element={<SobreNosotros />}></Route>
-            <Route path='/MetodosDePagos' element={<MetodosDePagos />}></Route>
-            <Route path='/MetodosDeEnvios' element={<MetodosDeEnvios />}></Route>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/:ProductosNombre"
+            element={<TarjetaEntera infoRopa={ListaProductos} />}
+          ></Route>
+          <Route path="/SobreNosotros" element={<SobreNosotros />} />
+          <Route path="/MetodosDePagos" element={<MetodosDePagos />} />
+          <Route path="/MetodosDeEnvios" element={<MetodosDeEnvios />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
 
 
 
